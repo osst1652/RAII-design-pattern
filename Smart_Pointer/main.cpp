a0a070069f5e6968a5ab77518198ef009d5fd5fa
osst1652 <oskar.steinhauf@gmail.com>
2017-04-03 21:20:50 +0200

    added lazy pointer implementation. Final commit.

--- Smart_Pointer/main.cpp
+++ Smart_Pointer/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "smartP.h"
+#include "smartP2.h"
 
 class Resource {
 public:
@@ -18,6 +19,13 @@ public:
 int main() {
 	//Create the pointer and init it with the resource object.
 	Ptr<Resource> pr(new Resource());
+	pr->dummy_func();
+
+	//create via lazy pointer
+	//useful when we are not sure if we want to point to something with the resource. 
+	//So it is not created until its required. 
+
+	LazyPointer<Resource> pr;
 	pr->dummy_func();
 
 
