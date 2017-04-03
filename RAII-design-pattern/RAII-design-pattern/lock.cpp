56d67e00e7163e6ff30fbe1f623dc9b6f47f72e6
osst1652 <oskar.steinhauf@gmail.com>
2017-04-03 16:48:28 +0200

    completed lock.cpp functionality

--- RAII-design-pattern/RAII-design-pattern/lock.cpp
+++ RAII-design-pattern/RAII-design-pattern/lock.cpp
@@ -1,1 +1,17 @@
 #include <mutex>
+#include <iostream>
+#include <exception>
+
+class lock {
+	std::mutex& mut;
+
+public:
+	lock(std::mutex& m) : mut(m) {
+		std::cout << "Locked" << std::endl;
+		mut.lock();
+	}
+	~lock() {
+		std::cout << "Unlocked" << std::endl;
+		mut.unlock();
+	}
+};
