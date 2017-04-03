f81257d1721716ea2a8ce59caf3c22858bb362eb
osst1652 <oskar.steinhauf@gmail.com>
2017-04-03 17:01:51 +0200

    Final commit

--- RAII-design-pattern/RAII-design-pattern/lock.cpp
+++ RAII-design-pattern/RAII-design-pattern/lock.cpp
@@ -1,17 +1,17 @@
-#include <mutex>
-#include <iostream>
-#include <exception>
-
-class lock {
-	std::mutex& mut;
-
-public:
-	lock(std::mutex& m) : mut(m) {
-		std::cout << "Locked" << std::endl;
-		mut.lock();
-	}
-	~lock() {
-		std::cout << "Unlocked" << std::endl;
-		mut.unlock();
-	}
-};
+//#include <mutex>
+//#include <iostream>
+//#include <exception>
+//
+//class lock {
+//	std::mutex& mut;
+//
+//public:
+//	lock(std::mutex& m) : mut(m) {
+//		std::cout << "Locked" << std::endl;
+//		mut.lock();
+//	}
+//	~lock() {
+//		std::cout << "Unlocked" << std::endl;
+//		mut.unlock();
+//	}
+//};
--- RAII-design-pattern/RAII-design-pattern/main.cpp
+++ RAII-design-pattern/RAII-design-pattern/main.cpp
@@ -1,3 +1,36 @@
+#include <mutex>
+#include <iostream>
+#include <exception>
+
+class lock {
+	std::mutex& mut;
+
+public:
+	lock(std::mutex& m) : mut(m) {
+		std::cout << "Locked" << std::endl;
+		mut.lock();
+	}
+	~lock() {
+		std::cout << "Unlocked" << std::endl;
+		mut.unlock();
+	}
+};
+
 int main() {
+	std::mutex mt;
+
+	std::cout << "start" << std::endl;
+
+	try {
+		lock lo(mt);
+
+		std::cout << "file locked" << std::endl;
+		throw std::exception();
+	}
+	catch (...) {
+		std::cout << "exception caught" << std::endl;
+	}
+	std::cout << "Over" << std::endl;
+	system("pause");
 	return 0;
 }
